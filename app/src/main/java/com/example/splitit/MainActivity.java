3451251ac64cb441b5e630cac60cc4cbc2c9b57d
Giacomo Grassetti <giacomograssetti@gmail.com>
2021-06-24 17:38:44 +0200

    added  login layout, topbar layout

--- app/src/main/java/com/example/splitit/MainActivity.java
+++ app/src/main/java/com/example/splitit/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.splitit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar topToolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.home);
     }
+
+
+
 }
