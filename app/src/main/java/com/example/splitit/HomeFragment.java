1bc323bec8c4f2ddbe78eadd44531d493911ff85
Tonelli Luca <luca.tonelli11@studio.unibo.it>
2021-06-29 18:02:29 +0200

    Added Fragment in gradle and created HomeFragment class

---
+++ app/src/main/java/com/example/splitit/HomeFragment.java
@@ -0,0 +1,12 @@
+package com.example.splitit;
+
+import android.widget.AdapterView;
+
+import androidx.fragment.app.Fragment;
+
+public class HomeFragment extends Fragment implements OnItemListener{
+    @Override
+    public void onItemClick(int position) {
+
+    }
+}
---
+++ app/src/main/java/com/example/splitit/OnItemListener.java
@@ -0,0 +1,8 @@
+package com.example.splitit;
+
+/**
+ * Interface to manage the listener for the click on an element of the RecyclerView
+ */
+public interface OnItemListener{
+    void onItemClick(int position);
+}
