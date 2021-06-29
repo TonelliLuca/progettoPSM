25cb5223e6ff2054fbfc4fe36a3fdc4cf9296b96
Giacomo Grassetti <giacomograssetti@gmail.com>
2021-06-29 17:47:04 +0200

    change in ConstrainLayout

--- app/src/main/java/com/example/splitit/LoginActivity.java
+++ app/src/main/java/com/example/splitit/LoginActivity.java
@@ -1,7 +1,6 @@
 package com.example.splitit;
 
 import android.content.Intent;
-import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
--- app/src/main/java/com/example/splitit/MainActivity.java
+++ app/src/main/java/com/example/splitit/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.splitit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ImageView;
 import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
