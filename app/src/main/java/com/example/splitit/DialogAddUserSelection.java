17d54a26e7c4b9201b586d8a24942da2307ba257
Giacomo Grassetti <giacomograssetti@gmail.com>
2021-07-30 00:35:53 +0200

    bug fix

--- app/src/main/java/com/example/splitit/DialogAddUserSelection.java
+++ app/src/main/java/com/example/splitit/DialogAddUserSelection.java
@@ -1,19 +1,32 @@
 package com.example.splitit;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.DialogFragment;
 
 import java.util.Objects;
 
 public class DialogAddUserSelection extends DialogFragment{
+
+    private EditText addCode;
+    private Button addQrCode;
+    private Button submit;
 
     @NonNull
     @Override
@@ -22,4 +35,21 @@ public class DialogAddUserSelection extends DialogFragment{
         View view = inflater.inflate(R.layout.add_user_selection, container, false);
         return view;
     }
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        final Activity activity = getActivity();
+        if (activity != null) {
+            addCode = view.findViewById(R.id.btn_add_code);
+            addQrCode = view.findViewById(R.id.btn_qr_reader);
+            submit = view.findViewById(R.id.btn_submit);
+
+            addQrCode.setOnClickListener(v -> {
+            });
+        }
+    }
+
+
+
+
 }
